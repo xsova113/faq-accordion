8d96b55f232b6fecdb35a37fd9af3a14ff17d4d1
George <xsova113@gmail.com>
2023-05-22 10:25:15 -0700

    refactor code using details/results elements

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,53 +1,57 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
 import data from "../libs/data";
 import { fadeIn } from "../utils/variants";
+import { useState } from "react";
 
 const Faq: React.FC = () => {
-  const [isClicked, setIsClicked] = useState(-1);
+  const [isClicked, setIsClicked] = useState([-1]);
+
+  const onClicked = (i: number) => {
+    const numArray: number[] = [];
+    if (numArray.includes(i)) {
+      numArray.splice(i, 1);
+      setIsClicked(numArray);
+      return;
+    }
+    setIsClicked(numArray);
+  };
 
   return (
-    <div className="flex flex-col sm:mt-[50px] -mt-[90px] sm:items-start items-center mx-8 pb-16">
+    <div className="flex flex-col sm:mt-[50px] -mt-[90px] sm:items-start items-center mx-8 pb-20">
       <h1 className="text-3xl font-bold mb-8">FAQ</h1>
       <div className="space-y-4 sm:text-[15px] text-[13px] flex flex-col">
         {data.map((item, i) => (
           <div key={i} className="flex justify-between gap-12 items-baseline">
-            <div className="flex flex-col w-full">
-              <h1
-                onClick={() => setIsClicked(i === isClicked ? -1 : i)}
-                className={`text-gray-600 active:text-black active:font-bold mb-2 ${
-                  isClicked === i && "text-black font-bold"
-                }`}
+            <details
+              className="flex flex-col w-full"
+              onClick={() => onClicked(i)}
+            >
+              <summary
+                style={{ listStyle: "none" }}
+                className={`flex justify-between gap-4 text-gray-600 active:text-black active:font-bold mb-2 focus:text-black focus:font-bold`}
               >
                 {item.q}
-              </h1>
+                <img
+                  src="/images/icon-arrow-down.svg"
+                  alt="arrow-icon"
+                  onClick={() => onClicked(i)}
+                  className={`w-3 h-3 ${
+                    isClicked.includes(i) && "rotate-180 transition"
+                  }`}
+                />
+              </summary>
 
-              {isClicked === i && (
-                <motion.div
-                  variants={fadeIn("left", "spring", 0, 1.2)}
-                  initial="hidden"
-                  whileInView="show"
-                >
-                  <p
-                    key={i}
-                    className="mb-3 font-thin text-[13px] text-gray-500"
-                  >
-                    {item.a}
-                  </p>
-                </motion.div>
-              )}
-              <hr className="my-1" />
-            </div>
-            <button
-              className=""
-              onClick={() => setIsClicked(i === isClicked ? -1 : i)}
-            >
-              <img
-                src="/images/icon-arrow-down.svg"
-                alt="arrow-icon"
-                className={`min-w-[13px] ${isClicked === i && "rotate-180"}`}
-              />
-            </button>
+              <hr className="my-2" />
+              <motion.div
+                variants={fadeIn("left", "spring", 0, 1.2)}
+                initial="hidden"
+                whileInView="show"
+              >
+                <p key={i} className="mb-3 font-thin text-[13px] text-gray-500">
+                  {item.a}
+                </p>
+              </motion.div>
+            </details>
           </div>
         ))}
       </div>
